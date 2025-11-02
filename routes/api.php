ae7633d5d2678025f5588bf1dc640480cc6bfaac
Veizaga <veizagamoralesjose@gmail.com>
2025-11-02 17:25:01 -0400

    update api

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+
+Route::get('/test', fn () => response()->json(['ok' => true]));
 Route::apiResource('users', UserController::class);
