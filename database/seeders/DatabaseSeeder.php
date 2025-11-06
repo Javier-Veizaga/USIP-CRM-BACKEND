2afe68a100d490b3ce686bb17c3cfef1e1c7cec6
Veizaga <veizagamoralesjose@gmail.com>
2025-11-06 08:42:50 -0400

    update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,8 @@ class DatabaseSeeder extends Seeder
             AgreementTypeSeeder::class,
             AgreementStatusSeeder::class,
             SchoolSeeder::class,
+            DataSourceSeeder::class,
+            CourseSeeder::class,
             
         ]);
     }
